6ca32053be3bd4586784097636de10844c42e9f8
Youness Makhfi <55758516+Younessmakhfi@users.noreply.github.com>
2020-12-09 16:58:49 +0100

    Add files via upload

---
+++ Medecin.java
@@ -0,0 +1,20 @@
+package exercice3;
+public class Medecin extends Personne{
+	
+	
+
+public Medecin(String nom, int age) {
+		super(nom, age);
+		// TODO Auto-generated constructor stub
+	}
+
+
+public void afficher() {
+	// TODO Auto-generated method stub
+	
+	 System.out.println("Je suis "+getNom()+"" +age+" ans et je travaille en tant que Medecin");
+	
+}
+
+	
+}
---
+++ Personne.java
@@ -0,0 +1,45 @@
+package exercice3;
+
+public class Personne {
+
+	private String nom;
+	int age;
+
+	
+	public Personne(String nom, int age) {
+		super();
+		this.nom = nom;
+		this.age = age;
+		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void afficher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	
+	
+	
+
+	
+	
+}
